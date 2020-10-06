1e3f741bdbe987ce527681f0f856cfa4588b8693
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2020-10-06 18:23:28 +0900

    MODIFY FILTER HEEEON 10/06 18:21

--- application/app/src/main/java/com/cheayoung/car_talk_app/ScanActivity.kt
+++ application/app/src/main/java/com/cheayoung/car_talk_app/ScanActivity.kt
@@ -28,16 +28,29 @@ class ScanActivity : AppCompatActivity() {
     var mScanSettings: ScanSettings.Builder? = null
     var scanFilters: MutableList<ScanFilter>? = null
     var simpleDateFormat = SimpleDateFormat("HH:mm:ss", Locale.KOREAN)
-    var push_state = 1
-    var sound_state = 1
-    var vibrate_state = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
-        if(intent.hasExtra("push")) push_state = intent.getIntExtra("push",1)
-        if(intent.hasExtra("sound")) sound_state = intent.getIntExtra("sound",1)
-        if(intent.hasExtra("vibrate")) vibrate_state = intent.getIntExtra("vibrate",1)
 
+        // 체크박스
+        button_search.setOnClickListener {
+            // 응급 알림만 수신
+            if(emergency_chk.isChecked == true){
+                if(mScanCallback.getInstance().data_list[01] == '01'){
+
+                }
+            }
+            // 사고 / 공사 알림만 수신
+            if(accident_chk.isChecked == true){
+
+            }
+            // 끼어들기 / 커브길 알림만 수신
+            if(etc_check.isChecked == true) {
+
+            }
+
+        }
+        // Scan
         ActivityCompat.requestPermissions(
             this, arrayOf<String>(
                 Manifest.permission.ACCESS_FINE_LOCATION,
@@ -52,11 +65,11 @@ class ScanActivity : AppCompatActivity() {
         mScanSettings = ScanSettings.Builder()
         mScanSettings!!.setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
         // 얘는 스캔 주기를 2초로 줄여주는 Setting입니다.
-        // 공식문서에는 위 설정을 사용할 때는 다른 설정을 하지 말고
-        // 위 설정만 단독으로 사용하라고 되어 있네요 ^^
-        // 위 설정이 없으면 테스트해 본 결과 약 10초 주기로 스캔을 합니다.
-
-        // 얘는 스캔 주기를 2초로 줄여주는 Setting입니다.
+        //        // 공식문서에는 위 설정을 사용할 때는 다른 설정을 하지 말고
+        //        // 위 설정만 단독으로 사용하라고 되어 있네요 ^^
+        //        // 위 설정이 없으면 테스트해 본 결과 약 10초 주기로 스캔을 합니다.
+        //
+        //        // 얘는 스캔 주기를 2초로 줄여주는 Setting입니다.
         // 공식문서에는 위 설정을 사용할 때는 다른 설정을 하지 말고
         // 위 설정만 단독으로 사용하라고 되어 있네요 ^^
         // 위 설정이 없으면 테스트해 본 결과 약 10초 주기로 스캔을 합니다.
@@ -83,7 +96,6 @@ class ScanActivity : AppCompatActivity() {
         setting.setOnClickListener {
             val intent = Intent(this, SettingActivity::class.java)// 다음 화면으로 이동
             startActivity(intent)
-            finish()
         }
     }
 
@@ -95,71 +107,74 @@ class ScanActivity : AppCompatActivity() {
         return dataBuilder.build()
     }
 
-    var mScanCallback: ScanCallback = object : ScanCallback() {
+    var mScanCallback: ScanCallback = object : ScanCallback(){
         @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-        override fun onScanResult(callbackType: Int, result: ScanResult) {
-            super.onScanResult(callbackType, result)
-            try {
-                val scanRecord: ScanRecord? = result.getScanRecord()
-                Log.d("getTxPowerLevel()", scanRecord?.getTxPowerLevel().toString() + "")
-                Log.d(
-                    "onScanResult()",
-                    result.getDevice().getAddress()
-                        .toString() + "\n" + result.getRssi() + "\n" + result.getDevice().getName()
-                            + "\n" + result.getDevice().getBondState() + "\n" + result.getDevice()
-                        .getType()
-                )
-                scanRecord!!.getServiceUuids()
-                val scanResult: ScanResult = result
-                // 4c 00 이후부터 뒤에 00 전까지 mManufacturerSpecificData
-                Thread {
-                    runOnUiThread {
-                        val change = scanRecord!!.toString()
-                        var ran1 = IntRange(change.indexOf("mManufacturerSpecificData")+27, change.indexOf("mServiceData")-4)
-                        var uuid_data = change.slice(ran1)
-                        var data_list = uuid_data.split(",")
-                        var ran2 = IntRange(0, data_list[0].indexOf("=")-1)
-                        var start_data = uuid_data.slice(ran2)
-                        var end_data : String
-                        var case_data = data_list[2]
-                        
-                        try {
-                            end_data = data_list[data_list.size - 1].slice(IntRange(1,3))
-                        }
-                        catch(e: Exception){
-                            end_data = "-1"
-                        }
-                        if(start_data == "76" && end_data == "-56" && data_list.size == 23) {
-                            beacon!!.add(
-                                0,
-                                Beacon(
-                                    scanResult.getDevice().getAddress(),
-                                    scanResult.getRssi(),
-                                    simpleDateFormat.format(Date()),
-                                    scanRecord!!
-                                )
-                            )
-                            // Adapter로 가기 전에 걸러줘야 할 것 같다.
-                            // 조건문 들어가야 되는 곳
-                            // state를 놓아서 경우마다 beacon 사용자 커스텀으로 볼 수 있도록 추가시켜주기
-                            // 알람 들어갈 함수
+                override fun onScanResult(callbackType: Int, result: ScanResult) {
+                    super.onScanResult(callbackType, result)
+                    try {
+                        val scanRecord: ScanRecord? = result.getScanRecord()
+                        Log.d("getTxPowerLevel()", scanRecord?.getTxPowerLevel().toString() + "")
+                        Log.d(
+                            "onScanResult()",
+                            result.getDevice().getAddress()
+                                .toString() + "\n" + result.getRssi() + "\n" + result.getDevice().getName()
+                                    + "\n" + result.getDevice().getBondState() + "\n" + result.getDevice()
+                                .getType()
+                        )
+                        scanRecord!!.getServiceUuids()
+                        val scanResult: ScanResult = result
+                        // 4c 00 이후부터 뒤에 00 전까지 mManufacturerSpecificData
+                        Thread {
+                            runOnUiThread {
+                                val change = scanRecord!!.toString()
+                                var ran1 = IntRange(change.indexOf("mManufacturerSpecificData")+27, change.indexOf("mServiceData")-4)
+                                var uuid_data = change.slice(ran1)
+                                var data_list = uuid_data.split(",")
+                                var ran2 = IntRange(0, data_list[0].indexOf("=")-1)
+                                var start_data = uuid_data.slice(ran2)
+                                var end_data : String
+                                try {
+                                    end_data = data_list[data_list.size - 1].slice(IntRange(1,3))
+                                }
+                                catch(e: Exception){
+                                    end_data = "-1"
+                                }
 
-                            beaconAdapter = BeaconAdapter(beacon, layoutInflater)
-                            beaconListView?.setAdapter(beaconAdapter)
-                            beaconAdapter?.notifyDataSetChanged()
 
-                        }
+                                if(start_data == "76" && end_data == "-56" && data_list.size == 23) {
+                                    beacon!!.add(
+                                        0,
+                                        Beacon(
+                                            scanResult.getDevice().getAddress(),
+                                            scanResult.getRssi(),
+                                            simpleDateFormat.format(Date()),
+                                            scanRecord!!
+                                        )
+                                    )
+
+
+                                    // Adapter로 가기 전에 걸러줘야 할 것 같다.
+                                    // 조건문 들어가야 되는 곳
+                                    // state를 놓아서 경우마다 beacon 사용자 커스텀으로 볼 수 있도록 추가시켜주기
+                                    // 알람 들어갈 함수
+
+                                    beaconAdapter = BeaconAdapter(beacon, layoutInflater)
+                                    beaconListView?.setAdapter(beaconAdapter)
+                                    beaconAdapter?.notifyDataSetChanged()
+                                    }
+                            }
+                        }.start()
+                    } catch (e: Exception) {
+                        e.printStackTrace()
                     }
-                }.start()
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
         }
+
+
 
         override fun onBatchScanResults(results: List<ScanResult?>) {
             super.onBatchScanResults(results)
             Log.d("onBatchScanResults", results.size.toString() + "")
-        }
+    }
 
         override fun onScanFailed(errorCode: Int) {
             super.onScanFailed(errorCode)
--- application/app/src/main/java/com/cheayoung/car_talk_app/SettingActivity.kt
+++ application/app/src/main/java/com/cheayoung/car_talk_app/SettingActivity.kt
@@ -1,6 +1,5 @@
 package com.cheayoung.car_talk_app
 
-import android.annotation.SuppressLint
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -8,65 +7,28 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.os.Bundle
-import android.view.View
-import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import com.kyleduo.switchbutton.SwitchButton
+import kotlinx.android.synthetic.main.activity_scan.*
 import kotlinx.android.synthetic.main.activity_setting.*
 
-
 class SettingActivity : AppCompatActivity() {
-    var push_state = 1
-    var sound_state = 1
-    var vibrate_state = 1
-
-    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
 
-        button_push_alam.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked -> // 스위치 버튼이 체크되었는지 검사하여 텍스트뷰에 각 경우에 맞게 출력합니다.
-            if (isChecked) {
-                push_state = 1
-            } else {
-                push_state = 0
-            }
-        })
-
-        button_sound.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked -> // 스위치 버튼이 체크되었는지 검사하여 텍스트뷰에 각 경우에 맞게 출력합니다.
-            if (isChecked) {
-                sound_state = 1
-            } else {
-                sound_state = 0
-            }
-        })
-
-        button_vibrate.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked -> // 스위치 버튼이 체크되었는지 검사하여 텍스트뷰에 각 경우에 맞게 출력합니다.
-            if (isChecked) {
-                vibrate_state = 1
-            } else {
-                vibrate_state = 0
-            }
-        })
-
         go_scan_page.setOnClickListener {
             val intent = Intent(this, ScanActivity::class.java)// 다음 화면으로 이동
-            intent.putExtra("vibrate",vibrate_state)
-            intent.putExtra("push",push_state)
-            intent.putExtra("sound",sound_state)
             startActivity(intent)
             finish()
         }
 
-        button_push_alam.setOnClickListener{
+        buttonPush.setOnClickListener{
             val NOTIFICATION_ID = 1001;
-            createNotificationChannel(
-                this, NotificationManagerCompat.IMPORTANCE_DEFAULT,
-                false, getString(R.string.app_name), "App notification channel"
-            ) // 1
+            createNotificationChannel(this, NotificationManagerCompat.IMPORTANCE_DEFAULT,
+                false, getString(R.string.app_name), "App notification channel") // 1
 
             val channelId = "$packageName-${getString(R.string.app_name)}" // 2
             val title =            "Car-Talk"
@@ -74,10 +36,8 @@ class SettingActivity : AppCompatActivity() {
 
             val intent = Intent(baseContext, MainActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            val pendingIntent = PendingIntent.getActivity(
-                baseContext, 0,
-                intent, PendingIntent.FLAG_UPDATE_CURRENT
-            )    // 3
+            val pendingIntent = PendingIntent.getActivity(baseContext, 0,
+                intent, PendingIntent.FLAG_UPDATE_CURRENT)    // 3
 
             val style = NotificationCompat.InboxStyle()
             style.addLine("Car no.1234 - Interfere")
@@ -101,10 +61,8 @@ class SettingActivity : AppCompatActivity() {
         }
     }
 
-    private fun createNotificationChannel(
-        context: Context, importance: Int, showBadge: Boolean,
-        name: String, description: String
-    ) {
+    private fun createNotificationChannel(context: Context, importance: Int, showBadge: Boolean,
+                                          name: String, description: String) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channelId = "${context.packageName}-$name"
             val channel = NotificationChannel(channelId, name, importance)
